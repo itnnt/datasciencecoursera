b152f7edfa05e43a2e2976fc1cf767933cebf689
itnnt <tung.nn83@gmail.com>
2016-07-11 22:21:49 +0700

    update code

--- programming-assignment-1-instructions-air-pollution/pollutantmean.R
+++ programming-assignment-1-instructions-air-pollution/pollutantmean.R
@@ -6,19 +6,24 @@
 ## pollutantmean("specdata", "nitrate", 23)
 ## [1] 1.281
 
-pollutantmean <- function(directory, pollutant, id=1:332){
+pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the csv files
   
-  ## 'pollutant' is a character vector of length 1 indicating 
-  ## the name of the pollutant for which we will calculate the 
+  ## 'pollutant' is a character vector of length 1 indicating
+  ## the name of the pollutant for which we will calculate the
   ## mean; either "sulfate" or "nitrate".
   
-  ## 'id' is an integer vector indicating the monitor ID numbers 
+  ## 'id' is an integer vector indicating the monitor ID numbers
   ## to be used
   
-  ## Return the mean of the pollutant across all monitors list 
+  ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
   ## NOTE: Do not round the result!
-  
+  total_pollutant <- 0
+  count_pollutant <- 0
+  for (i in id) {
+    filename <- paste0('00', i, '.csv')
+    print(substr(filename,nchar(filename)-6, nchar(filename)))
+  }
 }
